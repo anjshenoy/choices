e93f9688a4f1c0fefd6294f3be0e9f5704914216
Anjali Shenoy <anjshenoy@gmail.com>
2011-07-30 18:53:59 -0500

    changed single, value items to hashes instead of arrays of hashes

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -3,21 +3,27 @@ class Restaurant
   attr_reader :id, :single_items, :value_items
 
   def initialize(id, price, *items)
-    @id, @single_items, @value_items = id, [], []
+    @id, @single_items, @value_items = id, Hash.new(0), Hash.new(0)
     add_items(price, *items)
   end
 
 
   def add_items(price, *items)
     if items.size == 1
-      @single_items << {items.first => price}
+      @single_items[items.first] = price
     else
-      @value_items << {items => price}
+      @value_items[items] = price
     end
     if items.empty?
       raise ArgumentError.new("Data Error:: Item required in order to proceed")
     end
-    self
   end
 
+  def all_items
+    (@single_items.keys + @value_items.keys.flatten).uniq
+  end
+
+  def has_item?(item)
+    all_items.include?(item)
+  end
 end
--- spec/lib/restaurant_spec.rb
+++ spec/lib/restaurant_spec.rb
@@ -8,16 +8,12 @@ describe Restaurant do
     end
 
     it "has a list of single items with the price for each" do
-      Restaurant.new(1, 2.00, :burger).single_items.should == [{:burger => 2.00}]
+      Restaurant.new(1, 2.00, :burger).single_items.should == {:burger => 2.00}
     end
 
 
     it "has a list of value items with the price for each" do
-      Restaurant.new(1, 2.00, :burger, :fries, :drink).value_items.should == [{[:burger, :fries, :drink] => 2.00}]
-    end
-
-    it "throws an error if no items are provided" do
-      lambda{Restaurant.new(1, 2.00)}.should raise_error(ArgumentError, "Data Error:: Item required in order to proceed")
+      Restaurant.new(1, 2.00, :burger, :fries, :drink).value_items.should == {[:burger, :fries, :drink] => 2.00}
     end
   end
 
@@ -25,14 +21,38 @@ describe Restaurant do
     let(:r) { Restaurant.new(1, 2.00, :burger)} 
 
     it "can take on more single items" do
-      r.add_items(2.00, :fries).single_items.should == [{burger: 2.00}, {fries: 2.00}]
+      r.add_items(2.00, :fries)
+      r.single_items.should == {burger: 2.00, fries: 2.00}
     end
 
     it "can take on more value items" do
-      r.add_items(2.25, :burger, :fries).value_items.should == [{[:burger, :fries] => 2.25}]
+      r.add_items(2.25, :burger, :fries)
+      r.value_items.should == {[:burger, :fries] => 2.25}
     end
-    it "can take on more value items" do
+
+    it "can take on single and value items" do
+      r.add_items(2.00, :fries)
+      r.add_items(2.25, :burger, :fries)
+      r.single_items.should == {burger: 2.00, fries: 2.00}
+      r.value_items.should == {[:burger, :fries] => 2.25}
+    end
+
+    it "throws an error if no items are provided" do
       lambda{r.add_items(2.25)}.should raise_error(ArgumentError, "Data Error:: Item required in order to proceed")
     end
+  end
+
+  it "generates a list of every single unique item on the menu with no prices" do
+    r = Restaurant.new(1, 1.75, :fries)
+    r.add_items(2.00, :burger)
+    r.add_items(3.50, :burger, :fries)
+    r.add_items(1.00, :drink)
+    r.add_items(5.00, :fajitas, :drink, :fries)
+    r.all_items.should == [:fries, :burger, :drink, :fajitas]
+  end
+
+  it "returns true if it has a particular item" do
+    r = Restaurant.new(1, 2.00, :fries)
+    r.has_item?(:fries).should be_true
   end
 end
