7259a2835b39a1c9bd337b6dc6038ecec31513e6
Anjali Shenoy <anjshenoy@gmail.com>
2011-08-07 15:43:46 -0500

    Able to find the minimal price across multiple restaurants

--- spec/lib/choices_spec.rb
+++ spec/lib/choices_spec.rb
@@ -6,21 +6,21 @@ describe Choices do
   let(:choices) {Choices.new(1, 3.00, "burger")}
 
   context "initialization" do
-    it "creates a list of restaurants, each with a menu" do
+    it "initializes itself with a restaurant id and a menu item" do
       restaurants = Choices.new(1, 3.00, "burger").restaurants
       restaurants.size.should == 1
       restaurants.values.first.id.should == 1
-      restaurants.values.first.single_items.should == {"burger" => 3.00}
+      restaurants.values.first.line_items.should == {["burger"] => 3.00}
     end
   end
 
   context "add_items to one or more restaurants" do
-    it "can add any number of prices and menu items to each restaurant" do
+    it "can add any number of menu items to a restaurant" do
       choices.add_items(1, 2.00, "shake")
       restaurants = choices.restaurants
       restaurants.size.should == 1
       restaurants.values.first.id.should == 1
-      restaurants.values.first.single_items.should == {"burger" => 3.00, "shake" => 2.00}
+      restaurants.values.first.line_items.should == {["burger"] => 3.00, ["shake"] => 2.00}
     end
 
     it "can add items when the restaurant is new i.e. not already existing in the list" do
@@ -28,35 +28,32 @@ describe Choices do
       restaurants = choices.restaurants
       restaurants.size.should == 2
       restaurants.values.first.id.should == 1
-      restaurants.values.first.single_items.should == {"burger" => 3.00}
+      restaurants.values.first.line_items.should == {["burger"] => 3.00}
       restaurants.values.last.id.should == 2
-      restaurants.values.last.single_items.should == {"shake" => 2.00}
+      restaurants.values.last.line_items.should == {["shake"] => 2.00}
     end
 
     context "set up a restaurant with multiple single/value item menu options" do
-      it "can add single/value items for one restaurant" do
+      it "can add menu items for one restaurant" do
         choices.add_items(1, 1.50, "fries")
-        choices.add_items(1, 2.00, "shake")
         choices.add_items(1, 4.25, "burger", "fries")
-        choices.add_items(1, 3.25, "shake", "fries")
-        choices.restaurants.values.first.all_price_combinations.should include({"burger" => 3.00, "fries" => 1.50, "shake" => 2.00,
-                                                                               "burger, fries" => 4.25, "fries, shake" => 3.25})
+        choices.restaurants.values.first.line_items.should == {["burger"] => 3.00, 
+                                                               ["fries"] => 1.50, 
+                                                               ["burger", "fries"] => 4.25}
       end
 
-      it "can add single/value items for multiple restaurant" do
+      it "can add menu items for multiple restaurants" do
         choices.add_items(1, 1.50, "fries")
         choices.add_items(2, 2.00, "shake")
         choices.add_items(2, 4.25, "burger", "fries")
         choices.add_items(1, 3.25, "shake", "fries")
-        choices.restaurants.values.first.all_price_combinations.should include({"burger" => 3.00, "fries" => 1.50, "fries, shake" => 3.25})
-        choices.restaurants.values.last.all_price_combinations.should include({"shake" => 2.00, "burger, fries" => 4.25})
+        choices.restaurants.values.first.line_items.should == {["burger"] => 3.00, ["fries"] => 1.50, ["fries", "shake"] => 3.25}
+        choices.restaurants.values.last.line_items.should == {["shake"] => 2.00, ["burger", "fries"] => 4.25}
       end
     end
   end
 
-  # no matches
-  # multiple restaurants with the lowest price
-  context "price calculator - given a set of restaurants with single/value items" do
+  context "find the best price across multiple restaurants with multiple menu items each" do
     before(:each) do
       choices.add_items(1, 1.50, "fries")
       choices.add_items(1, 3.25, "shake", "fries")
@@ -65,23 +62,22 @@ describe Choices do
     end
 
     it "finds the most optimal price given a single item " do
-      choices.best_match("burger").should == {1 => 3.00}
+      choices.best_match("burger").should == "1, 3.0"
     end
 
     it "finds the most optimal price given a single item even if it exists as part of a value item menu" do
       choices.add_items(3, 6.00, "burger", "shake", "fries")
-      choices.best_match("fries").should == {1 => 1.50, 2 => 2.00, 3 => 6.00}
+      choices.best_match("fries").should == "1, 1.5"
     end
 
     it "finds the most optimal price given multiple items" do
-      choices.best_match("burger", "fries").should == {1 => 4.50, 2 => 4.25}
+      choices.best_match("burger", "fries").should == "2, 4.25"
     end
 
     it "finds the most optimal price given single and value items" do
       choices.add_items(3, 2.50, "burger")
       choices.add_items(3, 6.00, "burger", "shake", "fries")
-      choices.best_match("burger", "shake", "fries").should == {1 => 6.25, 3 => 6.00}
+      choices.best_match("burger", "shake", "fries").should == "3, 6.0"
     end
   end
-
 end
