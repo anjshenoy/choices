6094451de5d08b6093eeb61ac92915a7785ef5e7
Anjali Shenoy <anjshenoy@gmail.com>
2011-08-09 22:59:57 -0500

    optimized relevant matches

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -58,22 +58,26 @@ class Restaurant
   end
 
   # Find all menu items who's set intersection with order items is at least one
-  # Sort the result first by the size of items and then by price in ascending order
+  # Load the intersection as a hash key with the menu_item as the value.
+  # If another menu_item comes along matching the same intersection (i.e. key)
+  # with a lesser price, replace the hash value, otherwise move on.
+  # Sort the result by the highest intersecting set first
   # Thus the possibility of finding the lowest price first is maximized.
-  # Then remove items that have the same intersecting elements with order items but
-  # retain the one with the lowest price for this case. For example with:
-  # [[["burger", "fries"], 3.0], [["burger", "drink", "fries"], 5.0]]
+  # For example with: [[["burger", "fries"], 3.0], [["burger", "drink", "fries"], 5.0]]
   # for an order set of ["burger"] we only need the first one as that is the best
-  # price match for burger from all line items that intersect with burger
+  # price match for burger from all menu items that intersect with burger
   def relevant_matches(menu_items, order_items)
-    menu_items.select{|keys, price|
-      (keys & order_items).size > 0
-    }.sort{|a,b| b.first.size <=> a.first.size && a.last <=> b.last}.inject([]){|array, menu_item|
-      if array.empty? || !(menu_item.first & order_items == array.last.first & order_items && menu_item.last > array.last.last)
-        array << menu_item
+    hash = menu_items.inject({}){|result, menu_item|
+      intersection_set = menu_item.first & order_items
+      unless intersection_set.empty?
+        if result.has_key?(intersection_set) && menu_item.last < result[intersection_set].last
+          result[intersection_set] = menu_item
+        else
+          result[intersection_set] = menu_item
+        end
       end
-      array
-    }
+      result
+    }.sort{|a,b| b.last.first.size <=> a.last.first.size}.collect(&:last)
   end
 
   # The order items in the first argument are actually a clone
